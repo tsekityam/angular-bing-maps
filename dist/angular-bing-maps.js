eb359451c41a4d4a9b99d80a58a7afda08c44ffc
Stephen Morrow <smorrow@credera.com>
2015-02-02 11:08:45 -0600

    Added initial bing-map directive and example page

--- dist/angular-bing-maps.js
+++ dist/angular-bing-maps.js
@@ -23,3 +23,27 @@
       ]);
 
 })();
+
+/*global angular, Microsoft*/
+
+function bingMapDirective() {
+    'use strict';
+    
+    function link(scope, element, attrs) {
+        scope.bing = {};
+        scope.bing.map = new Microsoft.Maps.Map(element[0], {credentials: scope.credentials});
+    }
+    
+    return {
+        link: link,
+        template: '<div ng-transclude></div>',
+        restrict: 'EA',
+        transclude: true,
+        scope: {
+            credentials: '='
+        }
+    };
+
+}
+
+angular.module('angularBingMaps.directives').directive('bingMap', bingMapDirective);
