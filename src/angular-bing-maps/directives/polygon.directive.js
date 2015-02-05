bf9a4e6f229337ec233d5ff5afc8516d4cd6c500
Stephen Morrow <smorrow@credera.com>
2015-02-05 17:32:47 -0600

    Added Polygon directive

---
+++ src/angular-bing-maps/directives/polygon.directive.js
@@ -0,0 +1,61 @@
+/*global angular, Microsoft, DrawingTools, console*/
+
+function polygonDirective() {
+    'use strict';
+
+    function link(scope, element, attrs, mapCtrl) {
+        var bingMapLocations = [];
+        function generateBingMapLocations() {
+            bingMapLocations = [];
+            for (var i=0;i<scope.locations.length;i++) {
+                bingMapLocations.push(
+                    new Microsoft.Maps.Location(scope.locations[i].latitude, scope.locations[i].longitude)
+                );
+            }
+        }
+        generateBingMapLocations();
+
+        var polygon = new Microsoft.Maps.Polygon(bingMapLocations);
+        mapCtrl.map.entities.push(polygon);
+
+
+        function generateOptions() {
+            if(!scope.options) {
+                scope.options = {};
+            }
+            if (scope.fillColor) {
+                scope.options.fillColor = Microsoft.Maps.Color.fromHex(scope.fillColor);
+            }
+            if (scope.strokeColor) {
+                scope.options.strokeColor = Microsoft.Maps.Color.fromHex(scope.strokeColor);
+            }
+        }
+
+        scope.$watch('options', function (newOptions) {
+            polygon.setOptions(newOptions);
+        });
+        scope.$watch('locations', function() {
+            generateBingMapLocations();
+            polygon.setLocations(bingMapLocations);
+        });
+        scope.$watch('fillColor', generateOptions);
+        scope.$watch('strokeColor', generateOptions);
+    }
+
+    return {
+        link: link,
+        template: '<div ng-transclude></div>',
+        restrict: 'EA',
+        transclude: true,
+        scope: {
+            options: '=?',
+            locations: '=',
+            fillColor: '=?',
+            strokeColor: '=?'
+        },
+        require: '^bingMap'
+    };
+
+}
+
+angular.module('angularBingMaps.directives').directive('polygon', polygonDirective);
